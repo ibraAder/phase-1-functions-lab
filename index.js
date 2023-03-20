44f8ae25c805e0ad6fd867051dc7ea2b0059d49f
ibraAder <127183391+ibraAder@users.noreply.github.com>
2023-03-20 07:45:48 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    if(somevalue=43){
+        return 1;
+    }
+    else if(somevalue=50){
+        return 8;
+    }
+    else if(somevalue=34){
+        return 8;
+    }
+   
+}
+function distanceFromHqInFeet(FeetValue) {
+  if (FeetValue=43){
+    return 264;
+  }
+  else if(FeetValue=50){
+    return 2112;
+  }
+  else if(FeetValue=34){
+    return 2112;
+  }
+}
+function distanceTravelledInFeet(start, destination) {
+    const feetPerBlock =264;
+    const distanceBetweenBlocks=Math.abs(destination-start);
+    return distanceBetweenBlocks * feetPerBlock;
+
+}
+function calculatesFarePrice(start, destination) {
+const distanceFromHqInFeet=distanceTravelledInFeet(start,destination);
+if (distanceFromHqInFeet<=400){
+  return 0;
+}else if(distanceFromHqInFeet>400 && distanceFromHqInFeet<2000){
+  return(distanceFromHqInFeet-400)* 0.02;
+}
+else if(distanceFromHqInFeet>2000 && distanceFromHqInFeet<=2500){
+  return 25;
+}
+else{
+  return "Cannot travel that far";
+}
+}
